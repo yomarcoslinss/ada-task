719a9118e3b4a13832a08c2ce5b7ba69cee96845
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-10-19 23:59:15 -0300

    feat: adding card create

--- src/assets/js/drag-task.js
+++ src/assets/js/drag-task.js
@@ -34,3 +34,75 @@ function getNewPosition(column, posY) {
 
   return result;
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const addTaskButtons = document.querySelectorAll(".add-item");
+  const taskModal = document.getElementById("task-modal");
+  const closeTaskModal = taskModal.querySelector(".close");
+  const taskForm = document.getElementById("task-form");
+  const cardColumn = document.querySelector(".card-column");
+
+  $(function () {
+    $("#datepicker").datepicker({
+      minDate: 0,
+      dateFormat: "dd/mm/yy",
+    });
+  });
+
+  addTaskButtons.forEach(function (addTaskButton) {
+    addTaskButton.addEventListener("click", function () {
+      console.log("Clicou no botão de adicionar tarefa");
+      taskModal.style.visibility = "visible";
+    });
+  });
+
+  closeTaskModal.addEventListener("click", function () {
+    taskModal.style.visibility = "hidden";
+  });
+
+  taskForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+
+
+    const taskName = document.getElementById("task-name").value;
+    const taskPriority = document.getElementById("task-priority").value;
+    const taskDate = document.getElementById("datepicker").value;
+
+    // Crie uma nova div de item com os detalhes da tarefa
+    const newItem = document.createElement("div");
+    newItem.classList.add("item");
+    newItem.draggable = true;
+    newItem.innerHTML = `
+      <div class="card-name">
+        <p>${taskName}</p>
+        <div>
+          <i class="ph-thin ph-pencil-simple"></i>
+          <i class="ph-thin ph-trash"></i>
+        </div>
+      </div>
+      <div class="priority">
+        <div class="${taskPriority}">
+          <i class="ph-thin ph-warning-circle"></i>
+          <p>${taskPriority === "low" ? "Baixa" : taskPriority === "medium" ? "Média" : "Alta"}</p>
+        </div>
+        <div class="priority-date">
+          <i class="ph-thin ph-hourglass-medium"></i>
+          <p>${taskDate}</p>
+        </div>
+      </div>
+    `;
+
+    // Adicione o novo item à coluna de cartões
+    cardColumn.appendChild(newItem);
+
+    // Feche o modal
+    taskModal.style.visibility = "hidden";
+
+    // Limpe o formulário
+    taskForm.reset();
+  });
+});
+
